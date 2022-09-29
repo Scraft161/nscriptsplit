578a50edf69c49e00cffb543a2516e1b2450c7b6
scraft161 <scraft161@gmail.com>
2022-09-30 00:23:35 +0200

    v0.1 - it works!

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,14 @@ use std::{
 		File,
 	},
 	io::{
+		self,
+		prelude::*,
+		BufReader,
 		Error,
 		ErrorKind,
+		Write,
 	},
+	path::Path,
 };
 
 // We could string match, but regex makes this easier to adapt.
@@ -16,12 +21,8 @@ use regex::Regex;
 // Our set of changable constants
 const FILE_EXTENSION: &str = ".ns";
 const OUTPUT_DIR: &str = "script";
-//static REGEX_BLOCK_START: Result<regex::Regex, regex::Error> = Regex::new(r#"/\*(f|s)\d+"#);
 
 fn main() -> Result<(), std::io::Error> {
-	// Create block start regex
-	let regex_block_start = Regex::new(r#"/\*(f|s)\d+"#);
-
 	// Get file path from CLI
 	let args: Vec<String> = env::args().collect();
 
@@ -36,14 +37,16 @@ fn main() -> Result<(), std::io::Error> {
 	}
 
 	// Open file, since this may take a bit we notify the user.
-	print!("Reading file from disk, this may take a while... ");
+	print!("Reading file from disk... ");
+	io::stdout().flush().unwrap();
 
-	let source_nscript_file = fs::read_to_string(&args[1])?;
+	let source_nscript_file = File::open(&args[1])?;
 
 	println!("Done!");
 
 	// Split file and write the blocks, again we notify the user since this can take a bit
-	print!("Splitting file into blocks, this may take a while... ");
+	print!("Splitting file into blocks... ");
+	io::stdout().flush().unwrap();
 
 	split_file_into_blocks(source_nscript_file);
 
@@ -54,42 +57,73 @@ fn main() -> Result<(), std::io::Error> {
 }
 
 // Write each block into it's own file, F-[N] for F-blocks S-[N] for scenes/subroutines
-fn split_file_into_blocks(file: String) {
-	// Match starting blocks by regex
-	// For each block in file
-		// write_block_to_file(is_f_block, block_number)
+fn split_file_into_blocks(file: File) {
+	// Create top directory
+	fs::create_dir_all(OUTPUT_DIR).unwrap();
 
-	// Write the current block to file
-	write_block_to_file(true, 161, "I have mental issues");
+	let regex_block_start = Regex::new(r#"^\*(s|f)([\d]+)$"#).unwrap();
 
-	// Close the file gracefully to prevent the OS from locking the file after the program exits (edge case for windows, but always a good thing to do)
-	drop(file);
-	// PS: Rust ownership is a bit odd at times, just think of it that `source_nscript_file` became
-	// `file` after being passed to this function and that now neither exists anymore.
-	todo!();
+	let reader = BufReader::new(file);
+
+	// Values used for the current block
+	let mut is_f_block: bool = false;
+	let mut block_number: i32 = -1;	// Default value so we can catch malformed block identifiers
+	let mut block_data = String::new();
+
+	for line in reader.lines() {
+		let ln = line.unwrap().trim_end().to_string();
+		if regex_block_start.is_match(&ln) {
+			// Write last block if it contains data
+			if block_data != String::new() {
+				// Write file
+				write_block_to_file(is_f_block, block_number, &block_data);
+			}
+			// Get block type and id from match
+			for capture in regex_block_start.captures(&ln) {
+				//println!("block type: {}\nblock num:  {}", &capture[1], &capture[2]);
+
+				if &capture[1] == "f" {
+					is_f_block = true;
+				}
+
+				// get the block number as integer
+				block_number = capture[2].parse::<i32>().unwrap();
+			}
+
+			// Reset block data
+			block_data = String::new() + &(ln.to_string() + "\n");
+
+		} else {
+			block_data += &(ln.to_string() + "\n");
+		}
+	}
 }
 
 fn write_block_to_file(is_f_block: bool, block_number: i32, block_data: &str) {
-	dbg!(is_f_block);
-	dbg!(block_number);
-	dbg!(FILE_EXTENSION);
-	dbg!(OUTPUT_DIR);
+	let mut block_dir_path = OUTPUT_DIR.to_string() + "/";
 
-	let mut block_file_path = String::new();
-
-	// Rust version of a switch statement
+	// Rust version of a switch statement, we just use this to append in a readable way.
 	match is_f_block {
-		true => block_file_path = "F-Blocks/".to_string(),
-		false => block_file_path = "S-Blocks/".to_string()
+		true => block_dir_path += "F-Blocks/",
+		false => block_dir_path += "S-Blocks/",
 	}
 
-	// Create handler to the new file, this also creates the file itself
-	let mut block_file = File::create(block_file_path + &block_number.to_string() + FILE_EXTENSION)
+	// Create direcory if it doesn't exist
+	if ! Path::new(&block_dir_path).is_dir() {
+		fs::create_dir_all(&block_dir_path).unwrap();
+	}
+
+	let block_file_path = block_dir_path + &block_number.to_string() + FILE_EXTENSION;
+
+	// Create handler to the new file, this also creates the file itself if it doesn't exist yet.
+	let mut block_file = File::create(&block_file_path)
 		.expect("Could not create file for block {block_file_path}{block_number}{FILE_EXTENSION}");
 
-	dbg!(block_file);
-	// Close the file by dropping the handler, it should normally get dropped at the end of this
-	// function, but we want to avoid windows locking the file after the program exits.
-	//drop(block_file);
-	todo!();
+	// Finally write the current block to the file
+	block_file.write(block_data.as_bytes()).unwrap();
+
+	/*println!("[Debug]:
+	F-Block:      {is_f_block}
+	Block number: {block_number}
+	Block path:   {block_file_path}");*/
 }
